57e7ead40b69371cca4fc68dd46b593572d3e28c
sohag-9065 <sohaghossain7th@gmail.com>
2023-06-19 14:33:35 +0600

    server update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require('cors');
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +8,7 @@ app.use( cors() );
 app.use( express.json() );
 
 app.get('/', (req, res) => {
-    res.send("Hello  from Ai Productivity Master"); 
+ res.send("Hello  from Ai Productivity Master");
 })
 
 app.listen(port, () => {
